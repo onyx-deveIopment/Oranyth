6da0f9a0d0a51d4065061419c2646878c9cf3673
Andrew Cromar <90068691+AndrewCromar@users.noreply.github.com>
2025-01-20 18:52:04 -0800

    create rainbow effect

---
+++ Assets/_Project/Scripts/Effects/Effect_Rainbow.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class Effect_Rainbow : MonoBehaviour
+{
+    [Header("Debug")]
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private bool Enabled;
+
+    private void Start() => spriteRenderer = GetComponent<SpriteRenderer>();
+
+    private void Update() { if (Enabled) spriteRenderer.color = Color.HSVToRGB(Mathf.PingPong(Time.time * 0.5f, 1), 1, 1); }
+
+    public void Enable() => Enabled = true;
+    public void Disable() => Enabled = false;
+}

dba48864f6da71827cb84d9e01f0a618486675fa
Andrew Cromar <90068691+AndrewCromar@users.noreply.github.com>
2025-02-07 15:23:52 -0800

    sfx for time freeze powerup

--- Assets/_Project/Scripts/Collectible/Collectible_TimeFreeze.cs
+++ Assets/_Project/Scripts/Collectible/Collectible_TimeFreeze.cs
@@ -3,14 +3,19 @@ using UnityEngine;
 public class Collectible_TimeFreeze : Collectible
 {
     [Header("OBJECT ----------")]
+    [Header ("References")]
+    [SerializeField] private GameObject SFXPrefab_Powerup;
+
     [Header ("Setting")]
-    [SerializeField] private float duration = 5;
+    [SerializeField] private float Duration = 5;
     
     public override void OnCollected()
     {
         base.OnCollected();
 
-        GameController.Instance.FreezeTime = duration;
+        GameController.Instance.FreezeTime = Duration;
+
+        Instantiate(SFXPrefab_Powerup, transform.position, Quaternion.identity);
 
         base.ShowPopup("Time Freeze", transform.position);
         Destroy(gameObject);
